c995ea7132bfc9a395cc6a97b482f7362aa4bdf8
David <david.alenhag@outlook.com>
2022-06-08 23:36:09 +0200

    Create esp32_pins.h

---
+++ esp32_pins.h
@@ -0,0 +1,23 @@
+#ifndef _ESP32_PINS_H_
+#define _ESP32_PINS_H_
+
+#include "Arduino.h"
+
+const uint8_t BUTTON_pin = 2;
+
+const uint8_t IR_OUT_pin = 18;
+const uint8_t RF_REMOTE_pin = 19;
+
+const uint8_t SDA_pin = 21;
+const uint8_t SCL_pin = 22;
+
+const uint8_t LED_pin = 23;
+
+const uint8_t SSR_A_pin = 26;
+const uint8_t SSR_B_pin = 27;
+
+const uint8_t XTAL1_pin = 32;
+const uint8_t XTAL2_pin = 33;
+
+
+#endif // _ESP32_PINS_H_
